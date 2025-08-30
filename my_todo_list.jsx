27b052fa8ce8bf2a250bae699c35d66d3e522ca7
omarggaa <galalomar336@gmail.com>
2025-08-30 20:20:21 +0300

    Add files via upload

---
+++ my_todo_list.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: 'study' },
+    { id: 2, text: 'sleep' },
+    { id: 3, text: 'wake up' },
+
+  ]);
+  const [newTask, setNewTask] = useState('');
+
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
+  const addTask = (e) => {
+    e.preventDefault();
+    if (newTask.trim()) {
+      const newTaskObj = {
+        id: tasks.length + 1,
+        text: newTask
+      };
+      setTasks([...tasks, newTaskObj]);
+      setNewTask('');
+    }
+  };
+
+  return (
+    <div>
+      <h1>My To Do List</h1>
+      <input
+        type="text"
+        placeholder="Title..."
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+      />
+      <button onClick={addTask}>Add</button>
+
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.text}
+            <button onClick={() => deleteTask(task.id)}>x</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+import React, { useState } from 'react';
